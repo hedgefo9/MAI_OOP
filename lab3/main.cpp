d7201bbfde5c7acd54e7eba21e12fb4606265ad7
hedgefo9 <aleafedorov@mai.education>
2023-10-20 06:19:53 +0300

    lab3

---
+++ lab3/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+
+#include "include/Triangle.hpp"
+#include "include/Hexagon.hpp"
+#include "include/Octagon.hpp"
+#include "include/FigureList.hpp"
+
+int main() {
+
+    Triangle t1(Point(1, sqrt(3)), Point(2, 0), Point(0, 0));
+    Point p1 = t1.geom_center();
+    std::cout << t1 << std::endl;
+    std::cout << p1 << std::endl;
+    std::cout << t1.area() << std::endl << std::endl;
+
+    Triangle t2(2);
+    Point p2 = t2.geom_center();
+    std::cout << t2 << std::endl;
+    std::cout << p2 << std::endl;
+    std::cout << t2.area() << std::endl << std::endl;
+
+    Hexagon h1(3);
+    Point p3 = h1.geom_center();
+    std::cout << h1 << std::endl;
+    std::cout << p3 << std::endl;
+    std::cout << h1.area() << std::endl << std::endl;
+
+    Octagon o1(4);
+    Point p4 = o1.geom_center();
+    std::cout << o1 << std::endl;
+    std::cout << p4 << std::endl;
+    std::cout << o1.area() << std::endl << std::endl;
+
+    FigureList fl{};
+    fl.push_back(&t1);
+    fl.push_back(&t2);
+    fl.push_back(&h1);
+    fl.push_back(&o1);
+
+    fl.print_geom_center();
+    std::cout << fl.total_area() << std::endl;
+
+    return 0;
+}
